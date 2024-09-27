ce66b8c02ea5ec659a20019d652d6310c8df49e7
Ronald Pasion <52368740+RonaldPasion@users.noreply.github.com>
2024-09-27 14:53:20 -0700

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,3 +8,15 @@ const getUserChoice = userInput => {
     console.log('incorrect input');
   }
 }
+
+function getComputerChoice() {
+  const randomizer = Math.floor(Math.random() * 3);
+  switch (randomizer) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+}
